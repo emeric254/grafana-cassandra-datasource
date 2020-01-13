bf77c7e6e8aae71bc7a93e1ebda863d976baa47d
Emeric Tosi <emeric@3dtrust.fr>
2020-01-13 14:22:08 +0100

    import a modified simple-json-datasource to grafana-cassandra-datasource

---
+++ grafana-cassandra-datasource/src/datasource.js
@@ -0,0 +1,166 @@
+import _ from "lodash";
+
+export class GenericDatasource {
+
+  constructor(instanceSettings, $q, backendSrv, templateSrv) {
+    this.type = instanceSettings.type;
+    this.url = instanceSettings.url;
+    this.name = instanceSettings.name;
+    this.q = $q;
+    this.backendSrv = backendSrv;
+    this.templateSrv = templateSrv;
+    this.withCredentials = instanceSettings.withCredentials;
+    this.headers = {'Content-Type': 'application/json'};
+    if (typeof instanceSettings.basicAuth === 'string' && instanceSettings.basicAuth.length > 0) {
+      this.headers['Authorization'] = instanceSettings.basicAuth;
+    }
+  }
+
+  query(options) {
+    // used by panels to get data
+    // TODO
+    // TODO SELECT timestamp, name, value from value WHERE serial = $serial AND name IN () AND date IN $computedDates AND timestamp > $startTime AND timestamp < $endTime
+    var query = this.buildQueryParameters(options);
+    query.targets = query.targets.filter(t => !t.hide);
+
+    if (query.targets.length <= 0) {
+      return this.q.when({data: []});
+    }
+
+    if (this.templateSrv.getAdhocFilters) {
+      query.adhocFilters = this.templateSrv.getAdhocFilters(this.name);
+    } else {
+      query.adhocFilters = [];
+    }
+
+    console.log(query);
+
+    return this.doRequest({
+      url: this.url + '/query',
+      data: query,
+      method: 'POST'
+    });
+  }
+
+  testDatasource() {
+    // used by data source configuration page to make sure the connection is working
+    return this.doRequest({
+      url: this.url + '/health',
+      method: 'GET',
+    }).then(response => {
+      if (response.status === 200) {
+        return { status: "success", message: "Data source is working", title: "Success" };
+      }
+    });
+  }
+
+  annotationQuery(options) {
+    // used by dashboards to get annotations
+    // TODO
+    var query = this.templateSrv.replace(options.annotation.query, {}, 'glob');
+    var annotationQuery = {
+      range: options.range,
+      annotation: {
+        name: options.annotation.name,
+        datasource: options.annotation.datasource,
+        enable: options.annotation.enable,
+        iconColor: options.annotation.iconColor,
+        query: query
+      },
+      rangeRaw: options.rangeRaw
+    };
+
+    console.log(annotationQuery);
+
+    return this.doRequest({
+      url: this.url + '/annotations',
+      method: 'POST',
+      data: annotationQuery
+    }).then(result => {
+      return result.data;
+    });
+  }
+
+  metricFindQuery(query) {
+    // used by query editor to get metric suggestions.
+    // TODO
+    var interpolated = {
+        target: this.templateSrv.replace(query, null, 'regex')
+    };
+
+    console.log(interpolated);
+
+    return this.doRequest({
+      url: this.url + '/search',
+      data: interpolated,
+      method: 'POST',
+    }).then(this.mapToTextValue);
+  }
+
+  mapToTextValue(result) {
+    // TODO
+    return _.map(result.data, (d, i) => {
+      if (d && d.text && d.value) {
+        return { text: d.text, value: d.value };
+      } else if (_.isObject(d)) {
+        return { text: d, value: i};
+      }
+      return { text: d, value: d };
+    });
+  }
+
+  doRequest(options) {
+    // TODO
+    options.withCredentials = this.withCredentials;
+    options.headers = this.headers;
+
+    return this.backendSrv.datasourceRequest(options);
+  }
+
+  buildQueryParameters(options) {
+    // remove placeholder targets
+    options.targets = _.filter(options.targets, target => {
+      return target.target !== 'write your query here';
+    });
+
+    var targets = _.map(options.targets, target => {
+      return {
+        target: this.templateSrv.replace(target.target, options.scopedVars, 'regex'),
+        refId: target.refId,
+        hide: target.hide,
+        type: target.type || 'timeserie'
+      };
+    });
+
+    options.targets = targets;
+
+    return options;
+  }
+
+  getTagKeys(options) {
+    // TODO
+    return new Promise((resolve, reject) => {
+      this.doRequest({
+        url: this.url + '/tag-keys',
+        method: 'POST',
+        data: options
+      }).then(result => {
+        return resolve(result.data);
+      });
+    });
+  }
+
+  getTagValues(options) {
+    // TODO
+    return new Promise((resolve, reject) => {
+      this.doRequest({
+        url: this.url + '/tag-values',
+        method: 'POST',
+        data: options
+      }).then(result => {
+        return resolve(result.data);
+      });
+    });
+  }
+
+}
