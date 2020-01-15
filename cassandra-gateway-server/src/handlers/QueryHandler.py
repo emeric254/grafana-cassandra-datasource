690b6325aad16d6ee48e3f03ec7a2d6111016c3d
Emeric Tosi <emeric@3dtrust.fr>
2020-01-15 12:00:27 +0100

    add some robustness and code style to queryhandler

--- cassandra-gateway-server/src/handlers/QueryHandler.py
+++ cassandra-gateway-server/src/handlers/QueryHandler.py
@@ -2,6 +2,7 @@
 
 import logging
 from datetime import datetime
+from cassandra import DriverException, Unauthorized
 from tornado.escape import json_decode, json_encode
 from handlers.BaseHandler import BaseHandler
 from tools.CassandraClient import Client
@@ -23,78 +24,98 @@ class QueryHandler(BaseHandler):
         ):
             self.args = json_decode(self.request.body)
 
-    def post(self):
-
-        # TODO extract query from the request data
-        targets = self.args.get('targets')
-        if not targets:
-            self.set_status(400)
-            return
-
+    def _extract_time_range(self):
         time_range = self.args.get('range')
+
         if not time_range:
-            self.set_status(400)
-            return
+            return False
 
         raw_from = time_range.get('from')
         raw_to = time_range.get('to')
 
         if not raw_from or not raw_to:
-            self.set_status(400)
-            return
+            return False
 
         from_datetime = datetime.fromisoformat(raw_from.replace('Z', ''))
         to_datetime = datetime.fromisoformat(raw_to.replace('Z', ''))
 
-        start_time = str(int(from_datetime.timestamp() * 1e6))  # timestamp in microseconds
-        end_time = str(int(to_datetime.timestamp() * 1e6))  # timestamp in microseconds
-        # interval = str(self.args.get('intervalMs'))
+        self.start_time = str(int(from_datetime.timestamp() * 1e6))  # timestamp in microseconds
+        self.end_time = str(int(to_datetime.timestamp() * 1e6))  # timestamp in microseconds
+
+        return True
+
+    @staticmethod
+    def _parse_results(rows):
+        results = []
+
+        entries = {}
+
+        for row in rows:
+            timestamp = row.timestamp
+            name = row.name
+
+            if row.value is None:
+                continue
+
+            try:
+                value = float(row.value)
+            except ValueError or TypeError:
+                value = str(row.value)
+
+            if name not in entries:
+                entries[name] = {
+                    'target': row.name,
+                    'datapoints': [
+                        # an entry is: [ value (float), timestamp in milliseconds (int)]
+                    ]
+                }
+
+            entries[name]['datapoints'].append([
+                value,
+                timestamp / 1000  # convert our timestamp in microsecond into a millisecond one
+            ])
+
+        for entry in entries.values():
+            results.append(entry)
+
+        return results
+
+    def post(self):
+
+        targets = self.args.get('targets')
+
+        if not targets:
+            self.set_status(400)
+            return
+
+        if not self._extract_time_range():
+            self.set_status(400)
+            return
 
         results = []
 
         try:
             cassandra_client = Client.get_client()
-        except:
+        except DriverException:
             self.set_status(503)
             return
 
         for target in targets:
             request = target.get('target')
 
-            request = request.replace('$startTime', start_time)
-            request = request.replace('$endTime', end_time)
-            # request = request.replace('$interval', interval)
+            request = request.replace('$startTime', self.start_time)
+            request = request.replace('$endTime', self.end_time)
 
             if not request:
                 continue
 
-            rows = cassandra_client.execute(request)
+            try:
+                rows = cassandra_client.execute(request)
+            except Unauthorized:
+                self.set_status(403)
+                return
 
-            entries = {}
-
-            for row in rows:
-                timestamp = row.timestamp
-                name = row.name
-                try:
-                    value = float(row.value)
-                except ValueError:
-                    value = str(row.value)
-
-                if name not in entries:
-                    entries[name] = {
-                        'target': row.name,
-                        'datapoints': [
-                            # an entry is: [ value (float), timestamp in milliseconds (int)]
-                        ]
-                    }
-
-                entries[name]['datapoints'].append([
-                    value,
-                    timestamp / 1000  # convert our timestamp in microsecond into a millisecond one
-                ])
-
-            for entry in entries.values():
-                results.append(entry)
+            results = self._parse_results(rows)
 
         self.set_header('Content-Type', 'application/json')
         self.write(json_encode(results))
