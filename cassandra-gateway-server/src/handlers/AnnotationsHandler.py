2aaad2ad61bd06380d8a21c86dea6cb108e5d0b0
Emeric Tosi <emeric@3dtrust.fr>
2020-02-18 18:02:22 +0100

    merge duplicate code in a common handler

--- cassandra-gateway-server/src/handlers/AnnotationsHandler.py
+++ cassandra-gateway-server/src/handlers/AnnotationsHandler.py
@@ -1,48 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import logging
-from datetime import datetime
 from cassandra import DriverException, Unauthorized
-from tornado.escape import json_decode, json_encode
-from handlers.BaseHandler import BaseHandler
+from tornado.escape import json_encode
+from handlers.BaseQueryHandler import BaseQueryHandler
 from tools.CassandraClient import Client
 
 logger = logging.getLogger(__name__)
 
 
-class AnnotationsHandler(BaseHandler):
-
-    def data_received(self, _):
-        # we don't care about streamed data
-        pass
-
-    def prepare(self):
-        self.args = {}
-        if self.request.headers['Content-Type'] in (
-            'application/x-json',
-            'application/json'
-        ):
-            self.args = json_decode(self.request.body)
-
-    def _extract_time_range(self):
-        time_range = self.args.get('range')
-
-        if not time_range:
-            return False
-
-        raw_from = time_range.get('from')
-        raw_to = time_range.get('to')
-
-        if not raw_from or not raw_to:
-            return False
-
-        from_datetime = datetime.fromisoformat(raw_from.replace('Z', ''))
-        to_datetime = datetime.fromisoformat(raw_to.replace('Z', ''))
-
-        self.start_time = str(int(from_datetime.timestamp() * 1e6))  # timestamp in microseconds
-        self.end_time = str(int(to_datetime.timestamp() * 1e6))  # timestamp in microseconds
-
-        return True
+class AnnotationsHandler(BaseQueryHandler):
 
     @staticmethod
     def _parse_results(name: str, rows):
